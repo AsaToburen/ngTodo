cdb397f5ac6dd52adf21e7b0f3b29a8a0247b646
Asa Toburen <atopettit@gmail.com>
2015-07-13 14:55:55 -0700

    latest

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 var express = require('express'),
-  app = express(),
-  mongoose = require('mongoose'),
-  morgan = require('morgan'),
-  bodyParser = require('body-parser'),
-  methodOverride = require('method-override'),
-  port = process.env.PORT || 8080,
-  database = require('./config/database');
+    app = express(),
+    mongoose = require('mongoose'),
+    morgan = require('morgan'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    port = process.env.PORT || 8080,
+    database = require('./config/database');
 
 mongoose.connect(database.url);
 
 
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 app.use(express.static(__dirname + '/public'));
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({
-  'extended': 'true'
+    'extended': 'true'
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.json({
-  type: 'application/vnd.api+json'
+    type: 'application/vnd.api+json'
 }));
 app.use(methodOverride());
 
